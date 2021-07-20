20e879553ab92685be83657ceb92d57beacc14a1
Anuj Shaan <anujshaan@github.com>
2021-07-20 19:51:32 +0530

    email popup responsive

--- src/Components/GetInTouch/Get.jsx
+++ src/Components/GetInTouch/Get.jsx
@@ -24,7 +24,6 @@ export default function Get (){
                 trigger={buttonPopup} 
                 setTrigger = {setButtonPopup}
             >
-                
             </Popup>
             
         </div>
