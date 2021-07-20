0babd0ec82264d18c6a7368c170d1e7699d78ceb
Anuj Shaan <anujshaan@github.com>
2021-07-20 18:17:36 +0530

    email service

--- src/Components/Mailpopup/Popup.js
+++ src/Components/Mailpopup/Popup.js
@@ -1,17 +1,30 @@
 import React from 'react'
 import "./popup.css"
+import emailjs from 'emailjs-com';
+
 function Popup(props) {
+    const sendEmail = e =>{
+         e.preventDefault();
+        emailjs.sendForm('service_m9x7bbt', 'template_jvccugo', e.target, 'user_u1Nc5DVR3hVvjyZ03nrRz')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+      e.target.reset();
+    }
     return (props.trigger)?(
         <div className="popup">
             <div className="innerPopup">
                 <button className="closeBtn"
                     onClick={()=>props.setTrigger(false)}
                 >x</button>
-               <form action="">
-                    <input type="text"placeholder="Full name" />
-                    <input type="email" placeholder="Your Email" />
-                    <textarea name="" id="" cols="20" rows="5"></textarea>
-                    <button>Send</button>
+               <form onSubmit={sendEmail}>
+                    <input type="text"placeholder="Full name" name="name" required/>
+                    <input type="email" placeholder="Your Email" name="email" required/>
+                    <input type="text" placeholder="Subject" name="subject" required/>
+                    <textarea name="message" id="" cols="20" rows="5" required></textarea>
+                    <button type="submit">Send</button>
                 </form>
             </div>
         </div>
